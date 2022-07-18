e26157a1e23d8af3f1bef58d4d78f3c30fec2399
Diego Palma <dpalmasan@gmail.com>
2022-07-18 06:44:07 -0700

    Boilerplate code for wumpus GUI

---
+++ main.py
@@ -0,0 +1,35 @@
+import sys
+import pygame
+
+BLACK = (0, 0, 0)
+WHITE = (200, 200, 200)
+WINDOW_HEIGHT = 400
+WINDOW_WIDTH = 400
+
+
+def main():
+    global SCREEN, CLOCK
+    pygame.init()
+    SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    CLOCK = pygame.time.Clock()
+    SCREEN.fill(BLACK)
+
+    while True:
+        drawGrid()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+
+
+def drawGrid():
+    blockSize = 50 #Set the size of the grid block
+    for x in range(0, WINDOW_WIDTH, blockSize):
+        for y in range(0, WINDOW_HEIGHT, blockSize):
+            rect = pygame.Rect(x, y, blockSize, blockSize)
+            pygame.draw.rect(SCREEN, WHITE, rect, 1)
+
+
+main()

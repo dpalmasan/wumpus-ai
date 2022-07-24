58618c459206c975b8b356b33a76e744c36e67ae
Diego Palma <dpalmasan@gmail.com>
2022-07-24 12:18:46 -0700

    Add logic to draw an image on a Tile (#3)

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from abc import ABC, abstractmethod
 import sys
 import pygame
+from enum import Enum
+from typing import List
+from dataclasses import dataclass
+
+class Property(Enum):
+    BREEZE = 0
+    STENCH = 1
+    GOLD = 2
+    PIT = 3
+    WUMPUS = 4
+
+@dataclass
+class Position:
+    x: int
+    y: int
 
 BLACK = (0, 0, 0)
 WHITE = (200, 200, 200)
-WINDOW_HEIGHT = 400
-WINDOW_WIDTH = 400
+WINDOW_HEIGHT = 64*6
+WINDOW_WIDTH = 64*6
+
+breeze = pygame.transform.scale(pygame.image.load('breeze.png'), (21, 21))
 
 class Map:
     def __init__(self, block_size: int):
@@ -16,20 +33,25 @@ class Map:
         return self._block_size
 
     def draw(self):
-        blockSize = 50 #Set the size of the grid block
+        blockSize = 64
         for x in range(0, WINDOW_WIDTH, blockSize):
             for y in range(0, WINDOW_HEIGHT, blockSize):
-                rect = pygame.Rect(x, y, blockSize, blockSize)
-                pygame.draw.rect(SCREEN, BLACK, rect, 1)
+                tile = Tile(Position(x, y))
+                tile.draw(SCREEN)
 
 
-class Tile(ABC):
-    @abstractmethod
-    def draw(self) -> None:
-        raise NotImplemented()
-    
+class Tile:
+    def __init__(self, pos: Position, properties: List[Property] = [] , size =(64, 64)):
+        self._properties = properties
+        self._size = size
+        self._pos = pos
+        self._surface = pygame.Surface(size)
+        self._rect = pygame.Rect(pos.x, pos.y, *size)
 
 
+    def draw(self, canvas: pygame.Surface) -> None:
+        canvas.blit(breeze, self._rect)
+        pygame.draw.rect(canvas, BLACK, self._rect, 1)
 
 
 def main():
@@ -38,6 +60,7 @@ def main():
     SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     CLOCK = pygame.time.Clock()
     SCREEN.fill(WHITE)
+    breeze.convert()
 
     while True:
         map = Map(50)

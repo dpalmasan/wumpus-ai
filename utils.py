cdedce372171932380db357e398eef408f314288
Diego Palma <dpalmasan@fb.com>
2022-07-29 14:17:40 -0700

    Implement A* for shortest path problem (#10)

--- utils.py
+++ utils.py
@@ -1,11 +1,15 @@
 from dataclasses import dataclass
 import heapq
-from typing import Iterator, List, Optional
+from typing import Callable, Iterator, List, Optional
+
+DOWN, RIGHT, LEFT, UP = (0, 1), (1, 0), (-1, 0), (0, -1)
+ACTIONS = {DOWN: "down", RIGHT: "right", LEFT: "left", UP: "up"}
 
 
 class PriorityQueue:
     def __init__(self):
         self.heap = []
+        self.count = 0
 
     def empty(self):
         return len(self.heap) == 0
@@ -13,6 +17,7 @@ class PriorityQueue:
     def push(self, item, priority):
         entry = (priority, self.count, item)
         heapq.heappush(self.heap, entry)
+        self.count += 1
 
     def pop(self):
         (_, _, item) = heapq.heappop(self.heap)
@@ -33,11 +38,16 @@ class Position:
 
 class ShortestPathState:
     def __init__(
-        self, pos: Position, cost: int, prev: Optional["ShortestPathState"] = None
+        self,
+        pos: Position,
+        cost: int,
+        prev: Optional["ShortestPathState"] = None,
+        action: Optional[str] = None,
     ):
         self.pos = pos
         self.cost = cost
         self.prev = prev
+        self.action = action
 
     def __eq__(self, other):
         return self.pos == other.pos
@@ -62,7 +72,8 @@ class ShortestPathSearchProblem:
         m = len(self.visited)
         n = len(self.visited[0])
 
-        for x, y in ((0, 1), (1, 0), (-1, 0), (0, -1)):
+        for dir in (LEFT, DOWN, RIGHT, UP):
+            x, y = dir
             children = Position(state.pos.x + x, state.pos.y + y)
             cond = (
                 children.x >= 0
@@ -72,11 +83,43 @@ class ShortestPathSearchProblem:
                 and self.visited[children.y][children.x]
             )
             if cond:
-                yield ShortestPathState(children, state.cost + 1, state)
+                yield ShortestPathState(children, state.cost + 1, state, ACTIONS[dir])
 
 
-def a_star_route(problem: ShortestPathSearchProblem) -> List[str]:
-    pass
+def null_heuristic(
+    problem: ShortestPathSearchProblem, state: ShortestPathSearchProblem
+) -> int:
+    return 0
+
+
+def a_star_route(
+    problem: ShortestPathSearchProblem,
+    heuristic: Callable[
+        [ShortestPathSearchProblem, ShortestPathState], int
+    ] = null_heuristic,
+) -> List[str]:
+    pq = PriorityQueue()
+    start_state = problem.get_start_state()
+    pq.push(start_state, start_state.cost + heuristic(problem, start_state))
+    visited = set()
+    solution = []
+    while not pq.empty():
+        state = pq.pop()
+
+        if problem.is_goal_state(state):
+            while state:
+                if state.action:
+                    solution.insert(0, state.action)
+                state = state.prev
+            break
+
+        visited.add(state)
+
+        for child in problem.get_successors(state):
+            if child not in visited:
+                pq.push(child, child.cost + heuristic(problem, child))
+
+    return solution
 
 
 visited = [
@@ -85,7 +128,9 @@ visited = [
     [True, True, True, True],
     [True, True, True, True],
 ]
-problem = ShortestPathSearchProblem(Position(1, 1), Position(3, 3), visited)
+problem = ShortestPathSearchProblem(Position(1, 1), Position(2, 1), visited)
 start = problem.get_start_state()
 for children in problem.get_successors(start):
     print(children.pos)
+
+print(a_star_route(problem))

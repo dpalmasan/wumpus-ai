8fd885610db28d1f6ac943755ce56e81e836cc26
Diego Palma <dpalmasan@gmail.com>
2022-12-26 08:10:20 -0800

    (chore): Refactor logic from update

--- player.py
+++ player.py
@@ -1,6 +1,10 @@
 from abc import ABCMeta, abstractmethod
 from enum import Enum
 from pylogic import propositional
+
+import os
+
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 import pygame
 
 from utils import Point
@@ -15,7 +19,7 @@ class Direction(Enum):
 
 class Player(ABCMeta):
     @abstractmethod
-    def update(self, direction = Direction) -> None:
+    def update(self, direction=Direction) -> None:
         raise NotImplemented()
 
     @abstractmethod
@@ -33,7 +37,13 @@ class LogicAIPlayer(Player):
     def pos(self) -> Point:
         return self._pos
 
-    def update(self, direction: Direction):
+    def _run_plan(self):
+        for step in self._plan:
+            yield step
+
+    def update(self):
+
+        direction = next(self._plan)
         if direction == Direction.UP:
             self._pos.update(self._pos.x, self._pos.y - 1)
         if direction == Direction.DOWN:

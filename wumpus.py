469d88a019cac15944bc865a1f54387e02e7ac28
Diego Palma <dpalmasan@gmail.com>
2022-12-26 17:17:58 -0800

    [Wumpus] Add one wumpus rules (#20)

--- wumpus.py
+++ wumpus.py
@@ -8,48 +8,48 @@ class WumpusWorld:
     def __init__(self, grid):
         pass
 
-    def _one_wumpus_rule_1(self):
+    def _one_wumpus_rule_1(self, map_width: int = 4, map_height: int = 4):
         literals = set()
-        # TODO: Update for any size wumpus grid
-        for i in range(4):
-            for j in range(4):
-                literals.add(Variable(f"W{i}{j}"))
+        for i in range(map_width):
+            for j in range(map_height):
+                literals.add(Variable(f"W{i}{j}", truthyness=True))
 
         return CnfClause(literals)
 
-    def _one_wumpus_rule_2():
+    def _one_wumpus_rule_2(self, map_width: int = 4, map_height: int = 4):
         cnf_clauses = set()
-        for i in range(4):
-            for j in range(4):
+        for j in range(map_height):
+            for i in range(map_width):
                 if i > 0:
                     cnf_clauses.add(CnfClause([
                         Variable(
-                            f"W{i}{j}"
+                            f"W{i}{j}", truthyness=False
                         ),
-                        Variable(f"W{i - 1}{j}")
+                        Variable(f"W{i - 1}{j}", truthyness=False)
                     ]))
-                if i < 3:
+                if i < map_width - 1:
                     cnf_clauses.add(CnfClause([
                         Variable(
-                            f"W{i}{j}"
+                            f"W{i}{j}", truthyness=False
                         ),
-                        Variable(f"W{i + 1}{j}")
+                        Variable(f"W{i + 1}{j}", truthyness=False)
                     ]))
                 if j > 0:
                     cnf_clauses.add(CnfClause([
                         Variable(
-                            f"W{i}{j}"
+                            f"W{i}{j}", truthyness=False
                         ),
-                        Variable(f"W{i}{j - 1}")
+                        Variable(f"W{i}{j - 1}", truthyness=False)
                     ]))
-                if j < 3:
+                if j < map_height - 1:
                     cnf_clauses.add(CnfClause([
                         Variable(
-                            f"W{i}{j}"
+                            f"W{i}{j}", truthyness=False
                         ),
-                        Variable(f"W{i}{j + 1}")
+                        Variable(f"W{i}{j + 1}", truthyness=False)
                     ]))
         return cnf_clauses
-                
 
-
+wumpus_world = WumpusWorld([])
+print(wumpus_world._one_wumpus_rule_1())
+print(wumpus_world._one_wumpus_rule_2())
